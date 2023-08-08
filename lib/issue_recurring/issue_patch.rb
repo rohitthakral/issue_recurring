842fbd30ad2946fc4da5de443f748b01e566ccba
Vignesh <vemcse@gmail.com>
2023-08-08 19:41:58 +0530

    Bug fixing on the copy issue for additional tags compatible issue

--- lib/issue_recurring/issue_patch.rb
+++ lib/issue_recurring/issue_patch.rb
@@ -1,8 +1,24 @@
 module IssueRecurring
-  module CopyFromWithRecurrences
-    def copy_from(arg, options={})
-      super
+  # module CopyFromWithRecurrences
+  #   def copy_from(arg, options={})
+  #     # super
+  #     copy_from_with_tags(arg, options)
+  #     # self.copy_from_without_tags
+  #     unless options[:skip_recurrences]
+  #       self.recurrence_of = nil
 
+  #       if Setting.plugin_issue_recurring[:copy_recurrences]
+  #         self.recurrences = @copied_from.recurrences.map(&:dup)
+  #       end
+  #     end
+
+  #     self
+  #   end
+  # end
+
+  module InstanceMethods
+    def copy_from_with_recurring(arg, options={})
+      copy_from_without_recurring(arg, options={})
       unless options[:skip_recurrences]
         self.recurrence_of = nil
 
@@ -17,14 +33,16 @@ module IssueRecurring
 
   module IssuePatch
     Issue.class_eval do
-      prepend CopyFromWithRecurrences
+      # prepend CopyFromWithRecurrences
 
       has_many :recurrences, class_name: 'IssueRecurrence', dependent: :destroy
 
       belongs_to :recurrence_of, class_name: 'Issue', validate: true
       has_many :recurrence_copies, class_name: 'Issue', foreign_key: 'recurrence_of_id',
         dependent: :nullify
-
+      include InstanceMethods
+      alias_method :copy_from_without_recurring, :copy_from
+      alias_method :copy_from, :copy_from_with_recurring
       after_destroy :substitute_if_last_issue
     end
 
@@ -40,5 +58,6 @@ module IssueRecurring
       default_assign
     end
   end
+
 end
 
